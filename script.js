eef26d80cf0220a251c5f1a771642ad4ad696781
aidenkhoury <aidenkhoury04@gmail.com>
2024-10-28 15:37:53 +1100

    js test

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-let timerLoop = setInterval(function() {
-    let page_view_time = document.getElementById("time-container");
-    page_view_time = page_view_time + 1;
-}, 1000);
-
-timerLoop();
+document.getElementById("time-container") = 5;
